76894ee98b102f5659737414bc8f1a1af42ec5ab
markdavepanado <markdavepanado@gmail.com>
2021-03-20 20:14:30 +0800

    💄 todo action button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,36 @@
 import React from "react";
 import DoneIcon from "@material-ui/icons/Done";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 import "../styles/Todo.css";
 
 const Todo = ({ text, dateCreated, dateAccomplished, isDone }) => {
+  const styles = {
+    todoButtonIcon: {
+      fontSize: "1em",
+    },
+  };
+
   return (
     <div className={`todo ${isDone ? "todo--done" : ""}`}>
       <div className='todo__todo-date-container'>
         <div className='todo__action'>
-          <button className='todo__button todo__button--edit'>✏️ Edit</button>
-          <button className='todo__button todo__button--done'>✔️ Done</button>
+          <button className='todo__button todo__button--delete'>
+            <DeleteIcon style={styles.todoButtonIcon} />
+          </button>
+          {isDone ? (
+            ""
+          ) : (
+            <>
+              <button className='todo__button todo__button--edit'>
+                <EditIcon style={styles.todoButtonIcon} />
+              </button>
+              <button className='todo__button todo__button--done'>
+                <DoneIcon style={styles.todoButtonIcon} />
+              </button>
+            </>
+          )}
         </div>
         <div className='todo__date-container'>
           <small className='todo__date'>
